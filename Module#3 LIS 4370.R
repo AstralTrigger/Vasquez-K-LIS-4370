c1d82ff506eec7ecf14db881db0ae66778a34502
AstralTrigger <156020431+AstralTrigger@users.noreply.github.com>
2024-01-28 16:51:37 -0500

    Add files via upload
    R file for Module#3. This module goes over creating a dataframe using vectors of data from fictitious data based on the 2016 Presidential Election.

---
+++ Module#3 LIS 4370.R
@@ -0,0 +1,17 @@
+### Module 3 Assignment ###
+
+### Creating vectors that will store the 2016 presidential election data ###
+
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS <- c(12, 75, 43, 19, 1, 21, 19)
+
+### Merging vectors to create the dataframe ABC_v_CBS ###
+
+ABC_v_CBS <- data.frame(Name, ABC, CBS)
+
+### Viewing the dataframe result ###
+
+ABC_v_CBS
